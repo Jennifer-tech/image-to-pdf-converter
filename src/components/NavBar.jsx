96e524511278e6dcd792690e9d83355fb3043242
Jennifer-tech <muofunanyajennifer1@gmail.com>
2024-07-29 09:39:43 +0100

    worked on the functionality for converting an image or images to pdf

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React, { useContext } from 'react'
 import AuthContext from './stores/authContext'
 import { FaFilePdf } from "react-icons/fa";
 import { Link } from 'react-router-dom'
-// import Home from './Home';
+
 
 export default function NavBar() {
   const { user, login, logout, authReady } = useContext(AuthContext)
@@ -12,25 +12,29 @@ export default function NavBar() {
   return (
     <div className='h-80vh w-full border border-green-500'>
       <header>
-        <nav className='flex bg-blue-500 top-0 left-0 text-white h-14 w-full justify-between items-center px-10'>
+        <nav className='flex bg-blue-700 top-0 left-0 text-white h-14 w-full justify-between items-center px-10'>
           <div className='flex flex-row items-center space-x-1'>
-            <div className='w-[5] h-auto border border-green-700'>
-              <FaFilePdf className='w-[5] h-auto bg-blue-700' />
+            <div className='h-auto'>
+              <FaFilePdf className='w-9 h-auto bg-blue-700' />
               {/* <img src={pdfLogo} alt='img-to-pdf-converter logo' className='w-[5] h-auto'/> */}
             </div>
-            <h1 className='text-[4px] text-black'>Image To <br /> PDF Converter</h1>
+            <div className='flex flex-col items-center justify-center h-10'>
+              <h1 className='flex text-[9px] text-black font-primaryBold'>Image </h1>
+              <h1 className='flex text-[10px] text-white font-DancingScriptRegular'>To Pdf</h1>
+              <h1 className='flex text-[8px] text-black font-primaryBold'>Converter</h1>
+            </div>
           </div>
 
-          {/* <ul> */}
-          {/* <li><Link to='/' className='text-white'>Home</Link></li> */}
-          {authReady && (
-            <div className='space-x-2'>
-              {!user && <button onClick={login} className='bg-green-500 px-4 py-1 rounded-3xl text-sm'>Login/Signup</button>}
-              {user && <p className='text-sm'>{user.email}</p>}
-              {user && <button onClick={logout} className='bg-red-500 px-4 py-1 rounded-3xl text-sm'>Log out</button>}
-            </div>
-          )}
-          {/* </ul> */}
+          <ul>
+            <li><Link to='/' className='text-white font-primaryRegular'>Home</Link></li>
+            {authReady && (
+              <div className='space-x-2'>
+                {!user && <button onClick={login} className='bg-green-500 px-4 py-1 rounded-3xl text-sm font-primaryRegular'>Login/Signup</button>}
+                {user && <p className='text-sm'>{user.email}</p>}
+                {user && <button onClick={logout} className='bg-red-500 px-4 py-1 rounded-3xl text-sm font-primaryRegular'>Log out</button>}
+              </div>
+            )}
+          </ul>
 
         </nav>
       </header>
